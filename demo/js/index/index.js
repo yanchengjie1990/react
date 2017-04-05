aed484f6849c24dd886b3af9dab498a2aa079951
yanchengjie1990 <yanchengjie1990@163.com>
2017-04-05 20:59:52 +0800

    modify json

--- demo/js/index/index.js
+++ demo/js/index/index.js
@@ -1,5 +1,4 @@
-!function(t){function e(e,a){t(".not-container").hide();var e=n.filter(function(t){return t.type===a});e.length||t(".not-container").show(),t.typeHtml(e,"#index-list","#container-list")}var n=[],a=localStorage.getItem("type");t(".navContainer li").on("click",function(){var r=t(this);r.addClass("current").siblings().removeClass("current");var i=r.attr("data-type");localStorage.setItem("type",i),a=i,e(n,a)}),Handlebars.registerHelper("getStr",function(t){return t.substring(0,110)}),a&&t('.navContainer li[data-type="'+a+'"]').addClass("current").siblings().removeClass("current"),t(".small-menu").on("click",function(r){var i=r.target||window.event.srcElement;if(t(this).find(".sub-menu").toggle(),"A"==i.nodeName){var s=i.getAttribute("data-type");localStorage.setItem("type",s),a=s,e(n,a)}}),var r={
-	"data": [
+!function(t){function e(e,a){t(".not-container").hide();var e=n.filter(function(t){return t.type===a});e.length||t(".not-container").show(),t.typeHtml(e,"#index-list","#container-list")}var n=[],a=localStorage.getItem("type");t(".navContainer li").on("click",function(){var r=t(this);r.addClass("current").siblings().removeClass("current");var i=r.attr("data-type");localStorage.setItem("type",i),a=i,e(n,a)}),Handlebars.registerHelper("getStr",function(t){return t.substring(0,110)}),a&&t('.navContainer li[data-type="'+a+'"]').addClass("current").siblings().removeClass("current"),t(".small-menu").on("click",function(r){var i=r.target||window.event.srcElement;if(t(this).find(".sub-menu").toggle(),"A"==i.nodeName){var s=i.getAttribute("data-type");localStorage.setItem("type",s),a=s,e(n,a)}}),var r=[
 		{
 			"id": "0",
 			"type":"1",
@@ -105,5 +104,4 @@
 			"addTime": "2016-08-16",
 			"from": ""
 		}
-	]
-};var n=r.data,t.tmpHtml(r,"#index-list","#container-list"),a&&e(n,a)}(Zepto);
+	];var n=r,t.tmpHtml(r,"#index-list","#container-list"),a&&e(n,a)}(Zepto);
