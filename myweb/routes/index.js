24c7c4e2aa57add18eb44236312d2533fa46cc15
yanchengjie1990 <yanchengjie1990@163.com>
2017-05-07 19:58:22 +0800

    modify router floder of index.js

--- myweb/routes/index.js
+++ myweb/routes/index.js
@@ -4,24 +4,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var wxConfig = {
-		wechat: {
-			appID: 'wxb301ec2497baee6a',
-			appSecret: 'ab490017f78d9acdfb009275a44e7f1e',
-			token: '1688serviceyanchengjie'
+	console.log(this.query)
+	if (this.query) {
+		var wxConfig = {
+			wechat: {
+				appID: 'wxb301ec2497baee6a',
+				appSecret: 'ab490017f78d9acdfb009275a44e7f1e',
+				token: '1688serviceyanchengjie'
+			}
+		};
+		var token = wxConfig.wechat.token;
+		var signature = this.query.signature;
+		var nonce = this.query.nonce;
+		var timestamp = this.query.timestamp;
+		var echostr = this.query.echostr;
+		var str = [token, signature, nonce, timestamp, echostr].sort().join('');
+		var sha = sha1(str);
+		if (sha === signature) {
+			this.body = echostr + '';
+		} else {
+			this.body = 'wrong';
 		}
-	};
-	var token = wxConfig.wechat.token;
-	var signature = this.query.signature;
-	var nonce = this.query.nonce;
-	var timestamp = this.query.timestamp;
-	var ecostr = this.query.ecostr;
-	var str = [token, signature, nonce, timestamp, ecostr];
-	var sha = sha1(str);
-	if (sha === signature) {
-		this.body = ecostr + '';
-	} else {
-		this.body = 'wrong';
 	}
   res.sendfile("./public/demo/index.html");
 });
